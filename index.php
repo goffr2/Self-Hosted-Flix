c7a831ef797be69ab4a82cc9958cafccc17cb885
root <root@clinton.goff.net>
2020-10-11 20:32:11 -0400

    Created Login Page

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+
+	<head>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+
+	<body class="bg-light">
+		<nav class="navbar navbar-dark bg-dark">
+  			<a class="navbar-brand home" href="index.php">Self-Hosted-Flix</a>
+			<ul class="navbar-nav mr-auto">
+      			<li class="nav-item active">
+			<a class="nav-link" href="about.php">About</a>
+			</li>
+			</ul>
+			<form class="form-inline">
+				<div class="form-group">
+					<input type="username" class="form-control mr-1" placeholder="username" id="username">
+					<input type="password" class="form-control mr-1" placeholder="password" id="password">
+					<button type="submit" class="btn btn-secondary text-light">Sign In</button>
+				</div>
+		</nav>
+
+		
+		<div class="jumbotron jumbotron-fluid">
+
+			<div class="container">
+
+			<h3 class="text-center"> Welcome to Self-Hosted-Flix!</h3>
+
+
+        <!-- Top content -->
+        <div class="top-content">
+                <div class="container-fluid">
+                        <div id="carousel-example" class="carousel slide" data-ride="carousel">
+                                <div class="carousel-inner row w-100 mx-auto" role="listbox">
+                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3 active">
+                                                <div class="card mr-2" style="width: 15rem;"><img class="card-img-top" src="images/The_Dark_Knight.jpg" alt="Card image cap"></div>
+						</div>
+                                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                                                <div class="card mr-2" style="width: 15rem;"><img class="card-img-top" src="images/Outlaw_Star.jpg" alt="Card image cap"></div>
+						</div>
+                                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                                                <div class="card mr-2" style="width: 15rem;"><img class="card-img-top" src="images/See.jpg" alt="Card image cap"></div>
+						</div>
+                                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                                                <div class="card mr-2" style="width: 15rem;"><img class="card-img-top" src="images/Cowboy_bebop.jpg" alt="Card image cap"></div>
+						</div>
+                                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                                                <div class="card mr-2" style="width: 15rem;"><img class="card-img-top" src="images/The_Wolf_of_Wallstreet.jpg" alt="Card image cap"></div>
+						</div>
+                                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                                                <div class="card mr-2" style="width: 15rem;"><img class="card-img-top" src="images/Gone_Fishin.jpg" alt="Card image cap"></div>
+						</div>
+                                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                                                <div class="card mr-2" style="width: 15rem;"><img class="card-img-top" src="images/SAO.png" alt="Card image cap"></div>
+						</div>
+                                                <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
+                                                <div class="card mr-2" style="width: 15rem;"><img class="card-img-top" src="images/Trigun.jpg" alt="Card image cap"></div>
+						</div>
+                                </div>
+                        </div>
+                </div>
+        </div>
+
+
+			</div>
+
+
+		</div>
+
+	</body>
+
+                <script src="js/jquery-3.0.0.min.js"></script>
+                <script src="js/bootstrap.min.js"></script>
+                <script src="js/jquery.backstretch.min.js"></script>
+                <script src="js/wow.min.js"></script>
+
+               <script src="js/scripts.js"></script>
+
+</html>
